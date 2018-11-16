3fcfd64f4d5b87f45ec278bf4c39c5bbe715a1ad
Andy Zou <ml_zys@126.com>
2018-11-16 17:37:12 +0800

    im private conf

--- config/envConfig.js
+++ config/envConfig.js
@@ -13,10 +13,21 @@ let configMap = {
     online: {
       appkey: '45c6af3c98409b18a84451215d0bdd6e',
       url: 'https://app.netease.im'
+    },
+    privateConf: { 
+        "lbs_web": "http://127.0.0.1/lbs/webconf.jsp", 
+        "link_ssl_web": false, 
+        "nos_uploader_web": "", 
+        "https_enabled": false, 
+        "nos_downloader": "127.0.0.1/{bucket}/{object}", 
+        "nos_accelerate": "", 
+        "nos_accelerate_host": "", 
+        "nt_server": "" 
     }
   };
 ENVIRONMENT_CONFIG = configMap[envir];
 // 是否开启订阅服务
 ENVIRONMENT_CONFIG.openSubscription = true
-
+// 是否开启私有化部署
+ENVIRONMENT_CONFIG.openPrivateConf = false
 module.exports = ENVIRONMENT_CONFIG
